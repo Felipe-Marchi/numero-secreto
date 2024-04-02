d4a0aafb4a55d45f9d39dd848a09443b3660b0af
Felipe Marchi <felipemarchi20@gmail.com>
2024-04-02 16:49:02 -0300

    aumentando limite para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function exibirTexto(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTexto('h1', 'Jogo do número secreto');
-    exibirTexto('p', 'Escolha um número entre 1 e 10');
+    exibirTexto('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemInicial();
